b00014a11233723fa3e8804877403b88fbb402a2
leoelrojo <wiel.leo@gmail.com>
2020-05-18 15:35:30 +0200

    end of section 11

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense, editExpense, removeExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { setTextFilter, sortByAmount } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -18,14 +19,24 @@ store.subscribe(() => {
 
 store.dispatch(addExpense({
     description: 'Water bill',
-    amount: 40
+    amount: 4000
 }));
 
 store.dispatch(addExpense({
     description: 'Gas bill',
-    amount: 100
+    amount: 10000,
+    createdAt: 1000
 }));
 
-store.dispatch(setTextFilter('gas'));
+store.dispatch(addExpense({
+    description: 'Rent',
+    amount: 109500
+}));
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/components/PortfolioDetail.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const PortfolioDetail = (props) => (
-    <div>
-        Displaying portfolio {props.match.params.id}
-    </div>
-);
-
-export default PortfolioDetail;

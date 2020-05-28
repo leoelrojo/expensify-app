97c1df9c57eb316c43312a9cc1be1a4a6f0f22a1
leoelrojo <wiel.leo@gmail.com>
2020-05-28 19:13:34 +0200

    Setup formatting for amount and createdAt

--- src/app.js
+++ src/app.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import moment from 'moment';
+import localization from 'moment/locale/fr';
+import numeral from 'numeral';
 import { addExpense, editExpense, removeExpense } from './actions/expenses';
 import { setTextFilter, sortByAmount } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
+
+moment.updateLocale('fr', localization);
+
+//numeral.locale('fr');
 
 const store = configureStore();
 

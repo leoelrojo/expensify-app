992d165d0b8feabcbc46f087e16c7557f72d1c23
leoelrojo <wiel.leo@gmail.com>
2020-05-31 18:06:08 +0200

    end of section 13

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -2,36 +2,52 @@ import React from 'React';
 import { connect } from 'react-redux';
 import numeral from 'numeral';
 import selectSumExpenses from '../selectors/expenses-total';
+import selectExpenses from '../selectors/expenses';
 
-const statementToReturn = (props) => {
-    if(props.expenses.length === 0) {
+const statementToReturn = (expenses) => {
+    if(expenses.length === 0) {
         return "No expenses"
     } else {
         return (
-            props.expenses.length > 1 
-            ? `${props.expenses.length} expenses for ${
-                numeral(props.expenses.sum / 100)
+            expenses.length > 1 
+            ? `${expenses.length} expenses for ${
+                numeral(expenses.sum / 100)
                 .format('0.00$')
             }` 
-            : `${props.expenses.length} expense for ${
-                numeral(props.expenses.sum / 100)
+            : `${expenses.length} expense for ${
+                numeral(expenses.sum / 100)
                 .format('0.00$')
             }`
         )
     }
 };
 
-export const ExpenseTotal = (props) => ( 
-    <div>
+export const ExpenseTotal = ({ expenses }) => {
+    return (
+        <div>
         {
-            statementToReturn(props)
+            statementToReturn(expenses)
         }
     </div>
-);
+    )
+};
+
+// const ExpenseTotal = ({ expenseCount, expensesTotal }) => {
+//     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+//     const formattedExpensesSum = numeral(expensesTotal / 100).format('0.00$'');
+
+//     return (
+//         <div>
+//             <p>Viewing {expenseCount {expenseWord} for {formattedExpensesTotal}</p>
+//         </div>
+//     );
+// };
 
 const mapStateToProps = (state) => {
+    const visibleExpenses = selectExpenses(state.expenses, state.filters);
+    
     return {
-        expenses: selectSumExpenses(state.expenses)
+        expenses: selectSumExpenses(visibleExpenses)
     };
 };
 
--- src/tests/components/ExpenseTotal.test.js
+++ src/tests/components/ExpenseTotal.test.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseTotal } from '../../components/ExpenseTotal';
-import expenses from '../fixtures/expenses';
 
 test('should render statement with no expenses at all', () => {
-    const wrapper = shallow(<ExpenseTotal expenses={[]}/>);
+    const expenses =
+    {
+        sum: 0,
+        length: 0
+    };
+    const wrapper = shallow(<ExpenseTotal expenses={expenses}/>);
     expect(wrapper).toMatchSnapshot();
 });
 
 test('should render statement with only 1 expense (singular)', () => {
-    const wrapper = shallow(<ExpenseTotal expenses={expenses[0]}/>);
+    const expenses =
+    {
+        sum: 54,
+        length: 1
+    };    
+    const wrapper = shallow(<ExpenseTotal expenses={expenses}/>);
     expect(wrapper).toMatchSnapshot();
 });
 
 test('should render statement with the sum of expenses (plural)', () => {
+    const expenses =
+    {
+        sum: 1853,
+        length: 3
+    };    
     const wrapper = shallow(<ExpenseTotal expenses={expenses}/>);
     expect(wrapper).toMatchSnapshot();
 });

3bf8bc2f29f1bc645e9dab9f082c379b9adf0e15
leoelrojo <wiel.leo@gmail.com>
2020-06-21 17:19:19 +0200

    end of section 17

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,47 +1,54 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectSumExpenses from '../selectors/expenses-total';
 import selectExpenses from '../selectors/expenses';
 
 const statementToReturn = (expenses) => {
     if(expenses.length === 0) {
-        return "No expenses"
+        return <h1 className="page-header__title page-header__title--message">No expenses</h1>
     } else {
         return (
             expenses.length > 1 
-            ? `${expenses.length} expenses for ${
-                numeral(expenses.sum / 100)
-                .format('0.00$')
-            }` 
-            : `${expenses.length} expense for ${
-                numeral(expenses.sum / 100)
-                .format('0.00$')
-            }`
+            ? <h1 className="page-header__title"> 
+                <span> 
+                    {expenses.length} 
+                </span> 
+                { ' expenses for' } 
+                <span> {
+                    numeral(expenses.sum / 100)
+                    .format('0.00$')
+                }
+                </span>
+            </h1>
+            : <h1 className="page-header__title"> 
+                <span> 
+                    {expenses.length} 
+                </span> 
+                { ' expense for' } 
+                <span> {
+                    numeral(expenses.sum / 100)
+                    .format('0.00$')
+                }
+                </span>
+            </h1>
         )
     }
 };
 
 export const ExpenseTotal = ({ expenses }) => {
     return (
-        <div>
-        {
-            statementToReturn(expenses)
-        }
-    </div>
+        <div className="page-header">
+            <div className="content-container">
+                { statementToReturn(expenses) }
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
+        </div>
     )
 };
-
-// const ExpenseTotal = ({ expenseCount, expensesTotal }) => {
-//     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
-//     const formattedExpensesSum = numeral(expensesTotal / 100).format('0.00$'');
-
-//     return (
-//         <div>
-//             <p>Viewing {expenseCount {expenseWord} for {formattedExpensesTotal}</p>
-//         </div>
-//     );
-// };
 
 const mapStateToProps = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters);
